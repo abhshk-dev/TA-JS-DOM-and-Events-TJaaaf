b3f746bbbf93737b5c8ed488e4b8ed9650909bb6
Abhishek Agarwal <abhishekagarwal151@gmail.com>
2022-01-21 17:07:46 +0530

    Random backround generator updates

--- block-BJaaek/code/script.js
+++ block-BJaaek/code/script.js
@@ -1,9 +1,28 @@
 let clickMe=document.querySelector('.first');
 let mouseMove=document.querySelector('.second');
+
+//#ff0011
+// 0,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f
+function colorGenerator(){
+    let hexChars=[
+        '0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'];
+    let color='#';
+    for(let i=0;i<6;i++){
+        let randomNum=Math.floor(Math.random()*16);
+        color=color.concat(hexChars[randomNum]);
+    }
+    return color;
+}
+
 function changeColor(){
     let box=document.querySelector('.box');
-    box.style.backgroundColor="red";
+    box.style.backgroundColor=colorGenerator();
 }
 clickMe.addEventListener('click',changeColor);
 
-mouseMove.addEventListener('mousemove',changeColor);
+function handlemouseMove(){
+    let randomColor=colorGenerator();
+
+    mouseMove.style.backgroundColor=colorGenerator();
+}
+mouseMove.addEventListener('mousemove',handlemouseMove)
